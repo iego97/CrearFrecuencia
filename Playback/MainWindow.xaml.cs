b82217e8aefa06111c7177c045b94c2b963a3b69
iram_ <iram_@DESKTOP-FQDQHKI>
2018-02-16 11:41:20 -0700

    Slider Frecuencia

--- Playback/MainWindow.xaml.cs
+++ Playback/MainWindow.xaml.cs
@@ -31,6 +31,7 @@ namespace Playback
         DispatcherTimer timer;
         bool dragging = false;
         private VolumeWaveProvider16 volumeProvider;
+        SignalGenerator signalGenerator;
 
         public MainWindow()
         {
@@ -258,6 +259,48 @@ namespace Playback
                 writer.WriteSamples(buffer, 0, muestras);
             }
             writer.Dispose();
+        }
+
+        private void btnOffset_Click(object sender, RoutedEventArgs e)
+        {
+            var sampleRate = 44100;
+            var channelCount = 1;
+            var seconds = 10;
+            var signalGenerator = new SignalGenerator();
+            signalGenerator.Frequency = 750;
+            signalGenerator.Gain = 0.5;
+            var offsetProvider =
+                  new OffsetSampleProvider(signalGenerator);
+            offsetProvider.TakeSamples = sampleRate * seconds * channelCount;
+
+            WaveFileWriter.CreateWaveFile16("sonidito.wav", offsetProvider);
+        }
+
+        private void btnReproducirSeñal_Click(object sender, RoutedEventArgs e)
+        {
+            var sampleRate = 44100;
+            var channelCount = 1;
+            var seconds = 10;
+            signalGenerator = new SignalGenerator();
+            signalGenerator.Frequency = 750;
+            signalGenerator.Gain = 0.5;
+            var offsetProvider =
+                  new OffsetSampleProvider(signalGenerator);
+            offsetProvider.TakeSamples = sampleRate * seconds * channelCount;
+
+            output = new WaveOutEvent();
+            output.Init(signalGenerator);
+            output.Play();
+
+        }
+
+        private void sldFrecuencia_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if(sldFrecuencia != null && signalGenerator != null)
+            {
+                signalGenerator.Frequency = sldFrecuencia.Value;
+            }
+               
         }
     }
 }
